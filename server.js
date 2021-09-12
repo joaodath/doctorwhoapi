93002a6b28ca2463ade66184aaf737842a6362ef
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-09-12 11:48:44 -0300

    feature: added cors

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.json());
 const serverPort = process.env.PORT || 8001;
 
+//enabling CORS
+var cors = require("cors");
+app.use(cors());
+app.options("*", cors());
+
 app.use(routes);
 
 app.listen(serverPort, () => {
